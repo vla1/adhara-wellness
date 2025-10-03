46897a6df78748d1d3b70e62c18f2df07e1f3172
vla1 <vladis149@gmail.com>
2025-10-03 18:31:19 +0200

    apply font, fix eng text

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -9,7 +9,7 @@ import { LanguageService } from 'src/app/services/language.service';
 })
 export class HomeComponent implements OnInit, OnDestroy{
   title = 'adhara-beauty-wellness';
-  foto = '../../../assets/images/foto1.jpg'
+  foto = '../../../assets/images/adhara-images/Logo_adhara_azul.jpg'
   description = 'En Aldhara Beauty & Wellness, nos dedicamos a ofrecerte los mejores tratamientos estéticos y de bienestar. Nuestro equipo de profesionales está comprometido con tu cuidado y relajación, utilizando técnicas innovadoras y productos de alta calidad para garantizar tu satisfacción.';
   description2="Creemos en la belleza que viene de adentro y nos esforzamos por proporcionar un ambiente relajante donde puedas disfrutar de cada momento. Nuestro objetivo es ayudarte a sentirte bien contigo mismo, tanto por dentro como por fuera."
   description3="¡Te invitamos a visitarnos y descubrir la experiencia Aldhara!"
@@ -17,7 +17,6 @@ export class HomeComponent implements OnInit, OnDestroy{
   private languageSubscription: Subscription | undefined;
 
   constructor(private languageService:LanguageService){
-
   }
   ngOnInit() {
     // Suscribirse al cambio de idioma
