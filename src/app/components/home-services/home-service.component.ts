24ca0dcc9f170900860ca0cb94da654ea77a6b3b
vla1 <vladis149@gmail.com>
2025-10-03 20:07:10 +0200

    more translate

--- src/app/components/home-services/home-service.component.ts
+++ src/app/components/home-services/home-service.component.ts
@@ -1,5 +1,5 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { LanguageService } from 'src/app/services/language.service';
 
@@ -8,23 +8,36 @@ import { LanguageService } from 'src/app/services/language.service';
   templateUrl: './home-service.component.html',
   styleUrls: ['./home-service.component.scss']
 })
-export class HomeServiceComponent implements OnInit{
-  services: any = [];
-  private languageSubscription: Subscription | undefined;
+export class HomeServiceComponent implements OnInit, OnDestroy {
 
-  constructor(private http: HttpClient,
-          private languageService:LanguageService)
-          {}
+  // Variables a utilizar en el HTML
+  services: any[] = [];
+  sectionTitle = '';
+  buttonText = '';
 
-  ngOnInit(): void {
-    this.languageSubscription = this.languageService.getLanguageObservable().subscribe((lang) => {
-      this.loadServices(lang); // Cargar los datos del idioma seleccionado
+  private langSubscription: Subscription | undefined;
+
+  constructor(private http: HttpClient, private languageService: LanguageService) {}
+
+  ngOnInit() {
+    this.langSubscription = this.languageService.getLanguageObservable().subscribe(lang => {
+      this.loadServices(lang);
     });
   }
 
-  private loadServices(lang:string){
+  // FUNCIÓN AJUSTADA
+  private loadServices(lang: string) {
     this.http.get(`../../../assets/i18n/${lang}.json`).subscribe((data: any) => {
-      this.services = data.home.homeServices;
+      const homeData = data.home.homeServices;
+
+      // Asignar directamente a las variables del componente
+      this.services = homeData.services || [];
+      this.sectionTitle = homeData.sectionTitle;
+      this.buttonText = homeData.btnKnowMode;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.langSubscription?.unsubscribe();
   }
 }
